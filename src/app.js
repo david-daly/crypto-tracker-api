ef9a83a40877a03e328b48c6a8cbfecc3237c9f8
David Daly <david-daly@live.ie>
2018-05-02 23:10:31 +0100

    - Added babel preset to let me use es6 imports with `serverless-offline` - Add mongoose as a new dependency

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
-module.exports.handler = (event, context, callback) => {
+import mongoose from "mongoose";
+
+export const handler = (event, context, callback) => {
     context.succeed({ statusCode: 200, body: JSON.stringify('handler') });
 }
