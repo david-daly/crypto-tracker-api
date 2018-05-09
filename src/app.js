5b97417224e4f54468d2ae9ea8680fd8998ca1aa
David Daly <david-daly@live.ie>
2018-05-09 22:55:12 +0100

    refactoring -> re-order imports

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 var express = require('express');
-var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var router = express.Router();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 var path = require('path');
 var mongoose = require('mongoose');
 var cors = require('cors');
 var logger = require('morgan');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(logger('dev'));
 
 app.use(session({
     secret: 'session secret', //TODO: Fix this to make it more secure, remove from version control
@@ -16,21 +20,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
-app.use(cors());
-app.use(logger('dev'));
-
-// make index default route
 app.get('*', function (req, res) {
     return res.send('Hello World');
 });
-// start server
-var server = app.listen(3000, function () {
+
+const server = app.listen(5000, function () {
     console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 });
