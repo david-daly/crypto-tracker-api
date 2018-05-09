72638aa052e48dcfac3417fbf9cdf3cc34b02a3e
David Daly <david-daly@live.ie>
2018-05-09 23:06:45 +0100

    fix typo

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
-const environmnet = require('./config/environment')
+const environment = require('./config/environment')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(logger(environmnet.logger));
+app.use(logger(environment.logger));
 
 app.use(session({
     secret: 'session secret', //TODO: Fix this to make it more secure, remove from version control
@@ -25,6 +25,6 @@ app.get('*', function (req, res) {
     return res.send('Hello World');
 });
 
-const server = app.listen(environmnet.port, function () {
+const server = app.listen(environment.port, function () {
     console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 });
